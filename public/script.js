a01fc388fdfd648498ce0890b09c5481593a2bd1
Krable <39793454+Krable@users.noreply.github.com>
2023-03-09 17:24:04 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -230,4 +230,69 @@ inptSubmit.addEventListener("click", (e) => {
   }
 });
 
+const moviesUrl = "https://llsfsa.onrender.com/movies";
+
+// Fonction pour créer une affiche de film
+function createMoviePoster(movie) {
+  // Créer un élément div pour contenir l'affiche de film
+  const poster = document.createElement("div");
+  poster.classList.add("movie-poster");
+
+  // Ajouter l'image de l'affiche de film
+  const image = document.createElement("img");
+  image.src = movie.image_url;
+  image.alt = movie.title;
+  poster.appendChild(image);
+
+  // Ajouter le titre du film
+  const title = document.createElement("h2");
+  title.textContent = movie.title;
+  poster.appendChild(title);
+
+  // Ajouter la date de sortie du film
+  const releaseDate = document.createElement("p");
+  releaseDate.textContent = `Date de sortie : ${movie.release_date}`;
+  poster.appendChild(releaseDate);
+
+  // Ajouter la liste des plateformes de distribution
+  const distribution = document.createElement("ul");
+  if (movie.available_on.netflix) {
+    const netflix = document.createElement("li");
+    netflix.textContent = "Disponible sur Netflix";
+    distribution.appendChild(netflix);
+  }
+  if (movie.available_on.amazon) {
+    const amazon = document.createElement("li");
+    amazon.textContent = "Disponible sur Amazon";
+    distribution.appendChild(amazon);
+  }
+  if (movie.available_on.download) {
+    const download = document.createElement("li");
+    download.textContent = "Disponible en téléchargement";
+    distribution.appendChild(download);
+  }
+  poster.appendChild(distribution);
+
+  // Retourner l'élément div complet
+  return poster;
+}
+
+// Fonction pour créer des affiches de film pour chaque film de l'API
+function createMoviePosters(movies) {
+  // Sélectionner l'élément HTML où les affiches seront affichées
+  const posterContainer = document.getElementById("poster-container");
+
+  // Créer une affiche de film pour chaque film de l'API et l'ajouter au conteneur
+  movies.forEach((movie) => {
+    const poster = createMoviePoster(movie);
+    posterContainer.appendChild(poster);
+  });
+}
+
+// Appeler l'API pour récupérer la liste des films et créer des affiches pour chaque film
+fetch(moviesUrl)
+  .then((response) => response.json())
+  .then((movies) => createMoviePosters(movies))
+  .catch((error) => console.error(error));
+
 // title.className = "text title";
