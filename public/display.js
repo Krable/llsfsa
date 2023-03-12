442206aa7626c690450b8afd5417d122790b6fcf
leo <leolnvz334@gmail.com>
2023-03-12 22:45:16 +0100

    Refount partiel

--- public/display.js
+++ public/display.js
@@ -1,8 +1,9 @@
 const moviesUrl = "https://llsfsa.onrender.com/movies";
 
+const containerDipslay = document.getElementById("container_dipslay");
+
 // Fonction pour créer une affiche de film
 function createMoviePoster(movie) {
-  const containerDipslay = document.getElementById("container_dipslay");
   //
   // BOX DISPLAY
 
@@ -143,7 +144,7 @@ function createMoviePoster(movie) {
 // Fonction pour créer des affiches de film pour chaque film de l'API
 function createMoviePosters(movies) {
   // Sélectionner l'élément HTML où les affiches seront affichées
-  const posterContainer = document.getElementById("container_dipslay");
+  containerDipslay;
 
   // Créer une affiche de film pour chaque film de l'API et l'ajouter au conteneur
   movies.forEach((movie) => {
