fe91a533df629db843f0484cb91c5cf016062030
leo <leolnvz334@gmail.com>
2023-03-13 15:18:02 +0100

    Refount partiel

--- public/display.js
+++ public/display.js
@@ -132,6 +132,8 @@ function createMoviePoster(movie) {
   boxForbiden.appendChild(boxForbidenImage);
 
   containerDisplay.appendChild(boxDisplay);
+
+  return containerDisplay;
 }
 
 // Fonction pour créer des affiches de film pour chaque film de l'API
