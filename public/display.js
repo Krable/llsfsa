7fd1ed0904fc4155327ea1c0513ff40840a74b01
leo <leolnvz334@gmail.com>
2023-03-14 13:26:09 +0100

    Refount partiel

--- public/display.js
+++ public/display.js
@@ -32,7 +32,7 @@ function createMoviePoster(movie) {
 
   const secondTitle = document.createElement("p");
   secondTitle.classList.add("second_title");
-  secondTitle.textContent = movie.second_title;
+  secondTitle.textContent = movie.name2;
 
   let d = new Date(movie.release_date);
   let year = d.getFullYear();
@@ -136,7 +136,6 @@ function createMoviePoster(movie) {
   boxForbiden.appendChild(boxForbidenImage);
 
   containerDisplay.appendChild(boxDisplay);
-
   return containerDisplay;
 }
 
@@ -150,6 +149,27 @@ function createMoviePosters(movies) {
     const poster = createMoviePoster(movie);
     posterContainer.appendChild(poster);
   });
+
+  // SHEARCH BAR
+
+  const inptSearch = document.getElementById("inpt_search");
+
+  const allBoxDisplay = document.querySelectorAll(".box_display");
+
+  inptSearch.addEventListener("keyup", (e) => {
+    let recupValue = e.target.value.toLowerCase();
+
+    allBoxDisplay.forEach((uneBox) => {
+      const firstTitle = uneBox
+        .querySelector(".first_title")
+        .innerHTML.toLowerCase();
+      if (!firstTitle.includes(recupValue)) {
+        uneBox.classList.add("hiddenBoxDisplay");
+      } else {
+        uneBox.classList.remove("hiddenBoxDisplay");
+      }
+    });
+  });
 }
 
 // Appeler l'API pour récupérer la liste des films et créer des affiches pour chaque film
@@ -157,3 +177,103 @@ fetch(moviesUrl)
   .then((response) => response.json())
   .then((movies) => createMoviePosters(movies))
   .catch((error) => console.error(error));
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// FILTERS
+
+let x, i, j, l, ll, selElmnt, a, b, c;
+/* Look for any elements with the class "custom-select": */
+x = document.getElementsByClassName("custom-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  ll = selElmnt.length;
+  /* For each element, create a new DIV that will act as the selected item: */
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  /* For each element, create a new DIV that will contain the option list: */
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < ll; j++) {
+    /* For each option in the original select element,
+      create a new DIV that will act as an option item: */
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function (e) {
+      /* When an item is clicked, update the original select box,
+          and the selected item: */
+      var y, i, k, s, h, sl, yl;
+      s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+      sl = s.length;
+      h = this.parentNode.previousSibling;
+      for (i = 0; i < sl; i++) {
+        if (s.options[i].innerHTML == this.innerHTML) {
+          s.selectedIndex = i;
+          h.innerHTML = this.innerHTML;
+          y = this.parentNode.getElementsByClassName("same-as-selected");
+          yl = y.length;
+          for (k = 0; k < yl; k++) {
+            y[k].removeAttribute("class");
+          }
+          this.setAttribute("class", "same-as-selected");
+          break;
+        }
+      }
+      h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function (e) {
+    /* When the select box is clicked, close any other select boxes,
+      and open/close the current select box: */
+    e.stopPropagation();
+    closeAllSelect(this);
+    this.nextSibling.classList.toggle("select-hide");
+    this.classList.toggle("select-arrow-active");
+  });
+}
+
+function closeAllSelect(elmnt) {
+  /* A function that will close all select boxes in the document,
+    except the current select box: */
+  var x,
+    y,
+    i,
+    xl,
+    yl,
+    arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  xl = x.length;
+  yl = y.length;
+  for (i = 0; i < yl; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i);
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < xl; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+
+/* If the user clicks anywhere outside the select box,
+  then close all select boxes: */
+document.addEventListener("click", closeAllSelect);
